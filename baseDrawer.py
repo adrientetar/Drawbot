82672bb2ef6e97d782248da5dfa05d061ac882f3
Adrien Tétar <adri-from-59@hotmail.fr>
2015-03-26 22:12:36 +0100

    Tweak more

--- baseDrawer.py
+++ baseDrawer.py
@@ -2,9 +2,9 @@
 xHeight = 500
 ascender = 750
 descender = 250
-capHeight = 720
-baselineOverShoot = 15
-xHeightOverShoot = 15 
+capHeight = None
+baselineOverShoot = 12
+xHeightOverShoot = 12
 monospaceWidth = None
 
 
@@ -13,7 +13,7 @@ baseline = 220
 equis = (xHeight/10)*2.8346
 ascendente = (ascender/10)*2.8346
 descendente = (descender/10)*2.8346
-mayusculas = (capHeight/10)*2.8346
+if capHeight is not None: mayusculas = (capHeight/10)*2.8346
 baseOvershoot = (baselineOverShoot/10)*2.8346
 xOvershoot = (xHeightOverShoot/10)*2.8346
 if monospaceWidth is not None: monoWidth = (monospaceWidth/10)*2.8346
@@ -26,7 +26,7 @@ fontSize(8)
 text("base.(0)", (30, baseline-2))
 text("x–h(" +  str(xHeight) + ")", (30, (baseline+equis)-2))
 text("asc.(" +  str(ascender) + ")", (30, (baseline+ascendente)-2))
-text("cap.(" +  str(capHeight) + ")", (30, (baseline+mayusculas)-2))
+if capHeight is not None: text("cap.(" +  str(capHeight) + ")", (30, (baseline+mayusculas)-2))
 text("desc.(" +  str(descender) + ")", (30, (baseline-descendente)-2))
 text("1cm ≈ 100 upm", (30, 30))
 if monospaceWidth is not None: text("monospace: " + str(monospaceWidth) + " units", (30, 30))
@@ -52,7 +52,7 @@ rect(85, baseline+ascendente, 730, 0.3)
 rect(85, baseline-descendente, 730, 0.3)
 
 #cap height
-rect(85, baseline+mayusculas, 730, 0.3)
+if capHeight is not None: rect(85, baseline+mayusculas, 730, 0.3)
 
 #monospace guidelines
 if monospaceWidth is not None:
@@ -61,11 +61,11 @@ if monospaceWidth is not None:
     rect(85+padding+i*monoWidth, descendente-30, 0.2, 30+descendente+ascendente+30)
 
 #BaselineOvershoot
-fill(1, 0, 0, .2)
+fill(1, 0, 0, .1)
 rect(85, baseline, 730, -xOvershoot)
 
 #xHeightOverShoot
-fill(1, 0, 0, .2)
+fill(1, 0, 0, .1)
 rect(85, baseline+equis, 730, xOvershoot)
 
 #set a stroke color
